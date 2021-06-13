e235322cec5d035b94c3eea1ec240a3034718bfb
mohammad <mohammadhashemi42@gmail.com>
2021-06-13 19:17:39 +0430

    Add data annotations to ProfileViewModel

--- Blog.Web/ViewModels/ProfileViewModel.cs
+++ Blog.Web/ViewModels/ProfileViewModel.cs
@@ -9,7 +9,29 @@ namespace Blog.Web.ViewModels
 {
     public class ProfileViewModel
     {
-        [Display(Name = "تصویر پروفایل")]
-        public IFormFile Avatar { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [Display(Name = "نام کاربری")]
+        public string Username { get; init; }
+
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [MinLength(3, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [Display(Name = "نام مستعار")]
+        public string FriendlyName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "آدرس ایمیل")]
+        public string Email { get; init; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "تاریخ تولد")]
+        public DateTime? BirthDay { get; set; }
+
+        [Display(Name = "محل اقامت")]
+        public string Location { get; set; }
+
+        [Display(Name = "درباره من")]
+        public string Bio { get; set; }
     }
 }
