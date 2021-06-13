78f40ebe0f5f0444727fe4acfc236ecfe4369e42
mohammad <mohammadhashemi42@gmail.com>
2021-06-13 16:33:37 +0430

    update ProfileViewModel

--- Blog.Web/ViewModels/ProfileViewModel.cs
+++ Blog.Web/ViewModels/ProfileViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,7 @@ namespace Blog.Web.ViewModels
 {
     public class ProfileViewModel
     {
+        [Display(Name = "تصویر پروفایل")]
+        public IFormFile Avatar { get; set; }
     }
 }
