081c5d3f601257c1ef23f1d529bb75813178825e
mohammad <mohammadhashemi42@gmail.com>
2021-07-04 15:33:49 +0430

    Create ApplicationUserClaimsPrincipalFactory for adding avatar name to Cookie when user login

---
+++ Blog.Data/Identity/ApplicationUserClaimsPrincipalFactory.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Identity
+{
+    public class ApplicationUserClaimsPrincipalFactory
+        : UserClaimsPrincipalFactory<ApplicationUser>
+    {
+        public ApplicationUserClaimsPrincipalFactory(
+            UserManager<ApplicationUser> userManager,
+            IOptions<IdentityOptions> optionsAccessor)
+                : base(userManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+
+            if (string.IsNullOrWhiteSpace(user.AvatarName) is false)
+            {
+                identity.AddClaim(new Claim(ApplicationClaimTypes.Avatar, user.AvatarName));
+            }
+
+            return identity;
+        }
+    }
+}
