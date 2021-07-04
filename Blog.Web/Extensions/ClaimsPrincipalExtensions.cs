5613fc8537c3c49f923c46a46c43913b511b384c
mohammad <mohammadhashemi42@gmail.com>
2021-07-04 15:54:16 +0430

    Create ClaimsPrincipalExtensions

---
+++ Blog.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Data.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetAvatarName(this ClaimsPrincipal claimsPrincipal)
+        {
+            var avatarName = claimsPrincipal.Claims
+                .FirstOrDefault(c => c.Type == ApplicationClaimTypes.Avatar).Value;
+
+            return avatarName;
+        }
+    }
+}
