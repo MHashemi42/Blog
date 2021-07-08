6935862b8cf9507627936c04235d3baf4899ad28
mohammad <mohammadhashemi42@gmail.com>
2021-07-08 16:56:11 +0430

    Move PersianErrorDescriber class to Blog.Data.Identity namespace

--- Blog.Data/Identity/PersianErrorDescriber.cs
+++ Blog.Data/Identity/PersianErrorDescriber.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.Data
+namespace Blog.Data.Identity
 {
     public class PersianErrorDescriber : IdentityErrorDescriber
     {
