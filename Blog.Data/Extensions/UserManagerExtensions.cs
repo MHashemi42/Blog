d0900ce48327665b05315bfba9efcec60f8653c3
mohammad <mohammadhashemi42@gmail.com>
2021-07-12 11:56:03 +0430

    Add Extension method for UserManager for pagination

---
+++ Blog.Data/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data.Entities;
+using Blog.Data.Helpers;
+using Blog.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Extensions
+{
+    public static class UserManagerExtensions 
+    {
+        public static async Task<PagedList<ApplicationUser>> GetUsersAsync(
+            this UserManager<ApplicationUser> userManager,
+            ApplicationUserParameters parameters)
+        {
+            var source = userManager.Users;
+            var pagedList = await PagedList<ApplicationUser>
+                .ToPagedListAsync(source, parameters.PageNumber, parameters.PageSize);
+
+            return pagedList;
+        }
+    }
+}
