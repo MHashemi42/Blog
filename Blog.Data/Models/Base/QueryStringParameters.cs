08e8093a84fc511f0b5dd8d93761db7a950aec0e
mohammad <mohammadhashemi42@gmail.com>
2021-07-13 10:36:16 +0430

    Create QueryStringParameters abstract class

---
+++ Blog.Data/Models/Base/QueryStringParameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Models.Base
+{
+    public abstract class QueryStringParameters
+    {
+        const int maxPageSize = 20;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public string Query { get; set; }
+    }
+}
