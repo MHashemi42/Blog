aedc5f024ae60a372066549c14e4b46c915d5fe7
mohammad <mohammadhashemi42@gmail.com>
2021-07-16 08:31:15 +0430

    Create UserCreateViewModel

---
+++ Blog.Web/Areas/Admin/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Blog.Web.Attributes;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Areas.Admin.ViewModels
+{
+    public class UserCreateViewModel
+    {
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [MinLength(3, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [RegularExpression(@"^[a-zA-Z]([._-](?![._-])|[a-zA-Z0-9]){1,18}[a-zA-Z0-9]$",
+            ErrorMessage = "{0} نامعتبر است.")]
+        [Display(Name = "نام کاربری")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [MinLength(3, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [Display(Name = "نام مستعار")]
+        public string FriendlyName { get; set; }
+
+
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [EmailAddress(ErrorMessage = "{0} نامعتبر است.")]
+        [Display(Name = "آدرس ایمیل")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [MinLength(6, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "لطفا {0} خود را وارد نمائید.")]
+        [Compare(nameof(Password), ErrorMessage = "{0} با {1} مطابقت ندارد.")]
+        [Display(Name = "تکرار کلمه عبور")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "تاریخ تولد")]
+        public DateTime? BirthDay { get; set; }
+
+        [Display(Name = "محل اقامت")]
+        public string Location { get; set; }
+
+        [Display(Name = "درباره من")]
+        public string Bio { get; set; }
+
+        [AllowedExtensions(new string[] { ".jpg", ".png" })]
+        [Display(Name = "تصویر پروفایل")]
+        public IFormFile Avatar { get; set; }
+    }
+}
