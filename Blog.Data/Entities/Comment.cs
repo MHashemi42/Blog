aa20397054d60a78a47668e8791779d9288fd1ca
mohammad <mohammadhashemi42@gmail.com>
2021-07-17 11:36:35 +0430

    Add Comment entity

---
+++ Blog.Data/Entities/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string ChildrenIds { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+        public Comment Parent { get; set; }
+        public ApplicationUser User { get; set; }
+        public Post Post { get; set; }
+        public ICollection<Comment> Children { get; set; }
+    }
+}
