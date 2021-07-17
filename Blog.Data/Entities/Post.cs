b2408768a855cff0f5f41f7ae55ba8c71d8e660f
mohammad <mohammadhashemi42@gmail.com>
2021-07-17 09:06:02 +0430

    Add Post entity

---
+++ Blog.Data/Entities/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public bool IsHidden { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int AuthorId { get; set; }
+        public int? ModifierId { get; set; }
+
+        public ApplicationUser Author { get; set; }
+        public ApplicationUser Modifier { get; set; }
+    }
+}
