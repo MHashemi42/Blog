832ac8aabe9826ef2dc5f7072250c1c3f83b95a9
mohammad <mohammadhashemi42@gmail.com>
2021-07-17 10:26:08 +0430

    Add Label entity

---
+++ Blog.Data/Entities/Label.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Entities
+{
+    public class Label
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
