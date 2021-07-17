cbbd6416555204b2de0426d52fde6c699324f2aa
mohammad <mohammadhashemi42@gmail.com>
2021-07-17 11:43:24 +0430

    Update Comment entity

--- Blog.Data/Entities/Comment.cs
+++ Blog.Data/Entities/Comment.cs
@@ -11,7 +11,7 @@ namespace Blog.Data.Entities
         public int Id { get; set; }
         public int? ParentId { get; set; }
         public string ChildrenIds { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
         public int PostId { get; set; }
         public string Content { get; set; }
         public DateTime CreatedDate { get; set; }
