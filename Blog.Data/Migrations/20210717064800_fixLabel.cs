a257f26ce318c0ea5b525edde7bff1982ce71295
mohammad <mohammadhashemi42@gmail.com>
2021-07-17 11:18:47 +0430

    Migration

---
+++ Blog.Data/Migrations/20210717064800_fixLabel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class fixLabel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabelPost_Label_LabelsId",
+                table: "LabelPost");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Label",
+                table: "Label");
+
+            migrationBuilder.RenameTable(
+                name: "Label",
+                newName: "Labels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabelPost_Labels_LabelsId",
+                table: "LabelPost",
+                column: "LabelsId",
+                principalTable: "Labels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LabelPost_Labels_LabelsId",
+                table: "LabelPost");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels");
+
+            migrationBuilder.RenameTable(
+                name: "Labels",
+                newName: "Label");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Label",
+                table: "Label",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LabelPost_Label_LabelsId",
+                table: "LabelPost",
+                column: "LabelsId",
+                principalTable: "Label",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
