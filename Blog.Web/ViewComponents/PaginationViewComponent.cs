1ff7046c1483cb8f94b761903441f1d589d30f81
mohammad <mohammadhashemi42@gmail.com>
2021-07-18 11:33:31 +0430

    Create PaginationViewComponent

---
+++ Blog.Web/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewComponents
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(bool hasPrevious, bool hasNext, int currentPage)
+        {
+            string previousDisabled = hasPrevious ? string.Empty : "disabled";
+            string nextDisabled = hasNext ? string.Empty : "disabled";
+
+            return View((previousDisabled, nextDisabled, currentPage));
+        }
+    }
+}
