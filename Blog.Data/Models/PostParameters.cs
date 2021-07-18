c011980546dcb077f3a9948065550efb8c369e65
mohammad <mohammadhashemi42@gmail.com>
2021-07-18 11:09:15 +0430

    Create PostParameters

---
+++ Blog.Data/Models/PostParameters.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Data.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Models
+{
+    public class PostParameters : QueryStringParameters
+    {
+    }
+}
