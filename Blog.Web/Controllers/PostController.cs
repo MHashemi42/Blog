7841da19675f1b47752326eb30f027174a18194b
mohammad <mohammadhashemi42@gmail.com>
2021-07-21 09:23:59 +0430

    Add action for uploading image for CKEditor

--- Blog.Web/Controllers/PostController.cs
+++ Blog.Web/Controllers/PostController.cs
@@ -4,10 +4,13 @@ using Blog.Data.Models;
 using Blog.Data.Repositories;
 using Blog.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -17,10 +20,13 @@ namespace Blog.Web.Controllers
     public class PostController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public PostController(IUnitOfWork unitOfWork)
+        public PostController(IUnitOfWork unitOfWork,
+            IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<IActionResult> Index([FromQuery] PostParameters parameters)
@@ -63,6 +69,41 @@ namespace Blog.Web.Controllers
             await _unitOfWork.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin, Writer")]
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile upload)
+        {
+            if (upload is null)
+            {
+                return BadRequest();
+            }
+
+            var directoryPath = Path
+                .Combine(_webHostEnvironment.WebRootPath,"images", "posts");
+            if (Directory.Exists(directoryPath) is false)
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            var ext = Path.GetExtension(upload.FileName);
+            var fileName = Guid.NewGuid().ToString() + ext;
+            var filePath = Path.Combine(directoryPath, fileName);
+
+            using var stream = System.IO.File.Create(filePath);
+            await upload.CopyToAsync(stream);
+
+            var url = Path.Combine("/images", "posts", fileName);
+
+            var success = new
+            {
+                Uploaded = 1,
+                Url = url,
+                FileName = fileName
+            };
+
+            return Ok(success);
         }
     }
 }
