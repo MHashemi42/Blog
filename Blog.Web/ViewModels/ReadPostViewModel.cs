9be58c1b22f0284a58d6848773696682af471397
mohammad <mohammadhashemi42@gmail.com>
2021-07-21 10:36:42 +0430

    Create ReadPostViewModel

---
+++ Blog.Web/ViewModels/ReadPostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewModels
+{
+    public class ReadPostViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public HtmlString Body { get; set; }
+    }
+}
