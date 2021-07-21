d1f80da76d9a964b48a263c57ddc0cfa299a9dfa
mohammad <mohammadhashemi42@gmail.com>
2021-07-21 11:01:04 +0430

    Create UpdatePostViewModel

---
+++ Blog.Web/ViewModels/UpdatePostViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewModels
+{
+    public class UpdatePostViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "{0} باید بین {1} تا {2} حرف باشد.")]
+        [Required(ErrorMessage = "{0} مورد نیاز است.")]
+        [Display(Name = "عنوان")]
+        public string Title { get; set; }
+
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "{0} باید بین {1} تا {2} حرف باشد")]
+        [Required(ErrorMessage = "{0} مورد نیاز است.")]
+        [Display(Name = "توضیحات")]
+        public string Description { get; set; }
+
+        [MinLength(10, ErrorMessage = "{0} حداقل باید {1} حرف باشد.")]
+        [Required(ErrorMessage = "{0} مورد نیاز است.")]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "متن")]
+        public string Body { get; set; }
+
+        [Display(Name = "نمایش پست")]
+        public bool IsHidden { get; set; } = true;
+    }
+}
