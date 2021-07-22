94e0e013132c39494e279d88f41aead2ea13ed18
mohammad <mohammadhashemi42@gmail.com>
2021-07-22 08:29:24 +0430

    Create ILabelRepository

---
+++ Blog.Data/Repositories/LabelRepository.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repositories
+{
+    public class LabelRepository : Repository<Label>, ILabelRepository
+    {
+        public LabelRepository(BlogDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
