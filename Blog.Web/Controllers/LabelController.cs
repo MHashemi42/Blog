89fe50ff488a38498610fadd0c7d45324d4ffda8
mohammad <mohammadhashemi42@gmail.com>
2021-07-22 09:10:07 +0430

    Create LabelController

---
+++ Blog.Web/Controllers/LabelController.cs
@@ -0,0 +1,113 @@
+﻿using Blog.Data;
+using Blog.Data.Entities;
+using Blog.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Controllers
+{
+    public class LabelController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LabelController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var labels = await _unitOfWork.LabelRepository.GetAllAsync();
+
+            return View(labels);
+        }
+
+        [Authorize(Roles = "Admin, Writer")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin, Writer")]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateLabelViewModel viewModel)
+        {
+            if (ModelState.IsValid is false)
+            {
+                return View();
+            }
+
+            var isExist = await _unitOfWork.LabelRepository.IsExist(viewModel.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError(nameof(viewModel.Name), "نام برچسب تکراری است.");
+                return View();
+            }
+
+            Label label = new()
+            {
+                Name = viewModel.Name
+            };
+
+            await _unitOfWork.LabelRepository.AddAsync(label);
+            await _unitOfWork.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Admin, Writer")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var label = await _unitOfWork.LabelRepository.GetByIdAsync(id);
+            if (label is null)
+            {
+                return NotFound();
+            }
+
+            UpdateLabelViewModel viewModel = new()
+            {
+                Id = label.Id,
+                Name = label.Name
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize(Roles = "Admin, Writer")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, UpdateLabelViewModel viewModel)
+        {
+            if (ModelState.IsValid is false)
+            {
+                return View();
+            }
+
+            if (viewModel.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var label = await _unitOfWork.LabelRepository.GetByIdAsync(id);
+            if (label is null)
+            {
+                return NotFound();
+            }
+
+            var isExist = await _unitOfWork.LabelRepository.IsExist(viewModel.Name);
+            if (isExist)
+            {
+                ModelState.AddModelError(nameof(viewModel.Name), "نام برچسب تکراری است.");
+                return View();
+            }
+
+            label.Name = viewModel.Name;
+            await _unitOfWork.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Blog.Web/ViewModels/UpdateLabelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewModels
+{
+    public class UpdateLabelViewModel
+    {
+        public int Id { get; set; }
+
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "{0} باید بین {1} تا {2} حرف باشد.")]
+        [Required(ErrorMessage = "{0} مورد نیاز است.")]
+        [Display(Name = "نام برچسب")]
+        public string Name { get; set; }
+    }
+}
