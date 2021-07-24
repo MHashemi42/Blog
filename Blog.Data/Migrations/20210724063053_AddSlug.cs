a8208333d749a148cb2921e1bd50f243b5d16133
mohammad <mohammadhashemi42@gmail.com>
2021-07-24 11:06:44 +0430

    Migration

---
+++ Blog.Data/Migrations/20210724063053_AddSlug.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class AddSlug : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Posts",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Slug",
+                table: "Posts",
+                column: "Slug",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Slug",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Posts");
+        }
+    }
+}
