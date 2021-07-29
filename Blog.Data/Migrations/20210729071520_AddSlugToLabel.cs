4e5f543c216c3f1fbec76311215a03d6d85a4d76
mohammad <mohammadhashemi42@gmail.com>
2021-07-29 11:50:37 +0430

    Migration

---
+++ Blog.Data/Migrations/20210729071520_AddSlugToLabel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class AddSlugToLabel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Labels",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_Name",
+                table: "Labels",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Labels_Slug",
+                table: "Labels",
+                column: "Slug",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Labels_Name",
+                table: "Labels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Labels_Slug",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Labels");
+        }
+    }
+}
