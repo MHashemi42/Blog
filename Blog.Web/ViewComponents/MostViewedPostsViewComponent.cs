f52682e3f9cee59146c56ce71ed59611b77f3c1a
mohammad <mohammadhashemi42@gmail.com>
2021-07-31 13:02:31 +0430

    Create MostViewedPostsViewComponent

---
+++ Blog.Web/ViewComponents/MostViewedPostsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewComponents
+{
+    public class MostViewedPostsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MostViewedPostsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _unitOfWork.PostRepository
+                .GetMostViewedPostsAsync(postCount: 5);
+
+            return View(posts);
+        }
+    }
+}
