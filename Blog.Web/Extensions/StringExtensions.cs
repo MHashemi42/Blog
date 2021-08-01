42001bc1613dd14bc9ed3983a178a8908cf5180f
mohammad <mohammadhashemi42@gmail.com>
2021-08-01 13:25:13 +0430

    Create StringExtensions

---
+++ Blog.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        public static List<int> CommaSeparatedStringToIntList(this string str)
+        {
+            string[] numbersInStringArray = str.Split(',');
+            List<int> numbersInIntList = numbersInStringArray.Select(int.Parse).ToList();
+
+            return numbersInIntList;
+        }
+    }
+}
