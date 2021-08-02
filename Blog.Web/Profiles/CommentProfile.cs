f3d838be3d0449579a071dafc167ca4c9530f61c
mohammad <mohammadhashemi42@gmail.com>
2021-08-02 14:08:56 +0430

    Create profile to mapping Comment to ReadCommentViewModel

---
+++ Blog.Web/Profiles/CommentProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Blog.Data.Entities;
+using Blog.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Profiles
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<Comment, ReadCommentViewModel>();
+        }
+    }
+}
