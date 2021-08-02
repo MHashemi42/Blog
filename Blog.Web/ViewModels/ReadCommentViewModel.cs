98376f274a66a2a0883b0f2793e77a1d7b5bcae3
mohammad <mohammadhashemi42@gmail.com>
2021-08-02 14:08:08 +0430

    Create ReadCommentViewModel

---
+++ Blog.Web/ViewModels/ReadCommentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewModels
+{
+    public class ReadCommentViewModel
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string UserUserName { get; set; }
+        public string UserFriendlyName { get; set; }
+        public string UserAvatarName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Content { get; set; }
+        public IEnumerable<ReadCommentViewModel> Children { get; set; }
+    }
+}
