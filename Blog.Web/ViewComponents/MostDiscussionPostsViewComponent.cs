6c20f7b539a52809740bdf2bc9e6f045e46e9be7
mohammad <mohammadhashemi42@gmail.com>
2021-08-04 11:07:28 +0430

    Create MostDiscussionPostsViewComponent

---
+++ Blog.Web/ViewComponents/MostDiscussionPostsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.ViewComponents
+{
+    public class MostDiscussionPostsViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MostDiscussionPostsViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _unitOfWork.PostRepository
+                .GetMostDiscussionPostsAsync(postCount: 5);
+
+            return View(posts);
+        }
+    }
+}
