534ce8155278fdfbf174e1a07ddd860af2b8d167
mohammad <mohammadhashemi42@gmail.com>
2021-08-04 13:09:34 +0430

    Update PostSummary

--- Blog.Data/Models/PostSummary.cs
+++ Blog.Data/Models/PostSummary.cs
@@ -13,7 +13,7 @@ namespace Blog.Data.Models
         public string Title { get; set; }
         public string Slug { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
         public string AuthorFriendlyName { get; set; }
         public string AuthorUserName { get; set; }
         public IEnumerable<Label> Labels { get; set; }
