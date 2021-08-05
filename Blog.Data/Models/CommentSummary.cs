4c74c475647bd87b840d19bd78671e4b772c1587
mohammad <mohammadhashemi42@gmail.com>
2021-08-05 12:57:39 +0430

    Create CommentSummary and CommentParameters

---
+++ Blog.Data/Models/CommentSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Models
+{
+    public class CommentSummary
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string Content { get; set; }
+        public string UserFriendlyName { get; set; }
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostSlug { get; set; }
+    }
+}
