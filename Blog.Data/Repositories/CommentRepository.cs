a37acf55057f315a87f8a85defc3f2c1fcc51b9c
mohammad <mohammadhashemi42@gmail.com>
2021-08-05 12:57:56 +0430

    Update ICommnetRepository and CommentRepository

--- Blog.Data/Repositories/CommentRepository.cs
+++ Blog.Data/Repositories/CommentRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Blog.Data.Entities;
+using Blog.Data.Helpers;
+using Blog.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,6 +21,31 @@ namespace Blog.Data.Repositories
         {
             entity.CreatedDate = DateTime.UtcNow;
             return base.AddAsync(entity);
+        }
+
+        public Task<PagedList<CommentSummary>> GetUserCommentsPagedListAsync(CommentParameters parameters, int userId)
+        {
+            if (parameters is null)
+            {
+                throw new ArgumentNullException(paramName: nameof(parameters));
+            }
+
+            var source = _dbSet
+                .Where(c => c.UserId == userId)
+                .Select(c => new CommentSummary
+                {
+                    Id = c.Id,
+                    Content = c.Content,
+                    CreatedDate = c.CreatedDate,
+                    UserFriendlyName = c.User.FriendlyName,
+                    PostId = c.Post.Id,
+                    PostTitle = c.Post.Title,
+                    PostSlug = c.Post.Slug
+                })
+                .OrderByDescending(c => c.CreatedDate);
+
+            return PagedList<CommentSummary>
+                .ToPagedListAsync(source, parameters.PageNumber, parameters.PageSize);
         }
 
         public async Task<bool> IsExist(int commentId)
--- Blog.Data/Repositories/ICommentRepository.cs
+++ Blog.Data/Repositories/ICommentRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Blog.Data.Entities;
+using Blog.Data.Helpers;
+using Blog.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,6 @@ namespace Blog.Data.Repositories
     public interface ICommentRepository : IRepository<Comment>
     {
         Task<bool> IsExist(int commentId);
+        Task<PagedList<CommentSummary>> GetUserCommentsPagedListAsync(CommentParameters parameters, int userId);
     }
 }
